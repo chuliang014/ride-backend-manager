3c7a40399c9a3dac42a938658ac9b9d38d30a403
chuliang014 <chuliang014@gmail.com>
2020-08-07 19:45:35 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Life from './pages/demo/Life';
+import Admin from './admin';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Life />
+    <Admin />
   </React.StrictMode>,
   document.getElementById('root')
 );
