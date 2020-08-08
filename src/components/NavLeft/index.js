c4461a1b1745d7b8821351603f0766e7e47c542a
chuliang014 <chuliang014@gmail.com>
2020-08-07 20:25:43 -0700

    update: render aside menu

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Menu } from 'antd';
+import MenuConfig from '../../config/menuConfig';
+import './index.less';
+
+const SubMenu = Menu.SubMenu;
+
 
 export default class NavLeft extends React.Component {
+    UNSAFE_componentWillMount() {
+        const menuTreeNode = this.renderMenu(MenuConfig);
+        this.setState({
+            menuTreeNode
+        });
+    }
+
+    //menu render
+    renderMenu = (data) => {
+        return data.map((item) => {
+            if (item.children) {
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                );
+            }
+            return <Menu.Item title={item.title} key={item.key}> {item.title}</Menu.Item>
+        });
+    }
+
     render() {
         return (
             <div>
-                NavLeft
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt="" />
+                    <h1>Ride Manager</h1>
+                </div>
+                <Menu theme="dark">
+                    {this.state.menuTreeNode}
+                </Menu>
             </div>
         );
     }
